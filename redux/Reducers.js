2ad1a6dfc8be7b0506bd01758c135259f56fb6d5
Sam Corcos <sam@corcos.io>
2017-02-23 15:47:29 -0800

    feat: add reducer option

--- redux/Reducers.js
+++ redux/Reducers.js
@@ -6,6 +6,26 @@ export const AppReducer = (state = {}, action) => {
   }
 }
 
+// const initialFormState = {
+//   myForm: {
+//     input1: null,
+//   },
+// }
+//
+// export const FormsReducer = (state = initialFormState, action) => {
+//   switch (action.type) {
+//     case 'UPDATE_FORM':
+//       return {
+//         ...state,
+//         [action.payload.form]: {
+//           ...state[action.payload.form],
+//           [action.payload.key]: action.payload.value,
+//         },
+//       }
+//     default: return state
+//   }
+// }
+
 export const FormsReducer = (state = {}, action) => {
   switch (action.type) {
     case 'UPDATE_FORM':
